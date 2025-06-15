f9ec181c7610652027c7b8507163b53741fe7689
Annaliza Cabase <cabaseannaliza@gmail.com>
2025-06-15 08:22:14 +0800

    Update default layout watch route

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,3 +10,15 @@
 
   <PrimeScrollTop />
 </template>
+
+<script>
+const route = useRoute()
+const menuBar = useState('menuBar')
+
+watch(route, (newValue) => {
+  if (newValue) {
+    console.log(newValue)
+    menuBar.value = false
+  }
+}, { deep: true })
+</script>
