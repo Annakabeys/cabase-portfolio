6d31c426ca82e683e683dbe94e63cfca056330de
Annaliza Cabase <cabaseannaliza@gmail.com>
2025-06-15 11:48:02 +0800

    Update meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,24 @@ export default defineNuxtConfig({
   ],
   app: {
     head: {
+      title: 'Annaliza Cabase - Portfolio',
+      meta: [
+        {
+          name: 'description',
+          content: 'Explore the portfolio of Annaliza Cabase – Full Stack Developer specializing in Vue, Nuxt, and modern web technologies.'
+        },
+        { property: 'og:title', content: 'Annaliza Cabase – Portfolio' },
+        { property: 'og:description', content: 'Explore the portfolio of Annaliza Cabase – Full Stack Developer specializing in Vue, Nuxt, and modern web technologies.' },
+        { property: 'og:image', content: '/preview.png' },
+        { property: 'og:url', content: 'https://cabase-portfolio.vercel.app/' },
+        { property: 'og:type', content: 'website' },
+      ],
       link: [
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/favicon.png'
+        },
         {
           rel: 'stylesheet',
           href: 'https://cdn.jsdelivr.net/gh/devicons/devicon@v2.16.0/devicon.min.css'
